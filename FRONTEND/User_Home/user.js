0fc36763f9c644868bd2865fc078b7887e78d158
Kushal Bansal <321kushalbansal.kb@gmail.com>
2024-03-15 02:46:58 +0530

    added current location in user home

--- FRONTEND/User_Home/user.js
+++ FRONTEND/User_Home/user.js
@@ -19,3 +19,28 @@ document.getElementById('category').addEventListener('change', function () {
         this.parentNode.insertBefore(inputField, this.nextSibling);
     }
 });
+
+const locationSuccess = (coord)=>{
+    const locationError = document.getElementById("location-error")
+    const submit = document.getElementById("submit-btn")
+    
+    console.log(coord);
+    latitude = coord.coords.latitude
+    longitude = coord.coords.longitude
+    console.log(latitude,longitude);
+    
+    submit.removeAttribute("disabled")
+    locationError.removeAttribute()
+}
+
+const locationFail = ()=>{
+    const form = document.getElementById("form-container")
+    const locationError = document.createElement("div")
+    console.log("failed to get the location")
+    
+    locationError.id = "location-error"
+    locationError.innerHTML = `<p style="color: red;">**Note: Enable the location permission</p>`
+    form.appendChild(locationError)
+}
+
+navigator.geolocation.getCurrentPosition((coord)=>locationSuccess(coord),()=> locationFail())
