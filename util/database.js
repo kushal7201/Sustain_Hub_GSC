4bd8dc1e301cd999e225e44aa4cb379ef37e7d54
sugam <ssugamlm10@gmail.com>
2024-03-14 22:53:13 +0530

    colour added

--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('sustained', 'root', 'sugam112', {
-    dialect: 'mysql',
-    host: 'localhost'
-});   
-
-module.exports = sequelize;
-
